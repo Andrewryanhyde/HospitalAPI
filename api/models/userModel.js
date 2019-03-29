2d405c33ea9f63dfa7f60b097b012e84cdb426d6
Andrewryanhyde <andrewryanhyde@hotmail.co.uk>
2019-03-29 16:52:18 +0000

    Added User Schema

---
+++ api/models/userModel.js
@@ -0,0 +1,41 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema({
+  profilePicture: {
+    type: Buffer,
+    required: 'Please upload a profile picture.'
+  },
+  firstName: {
+    type: String,
+    required: 'Please enter a first name.'
+  },
+  lastName: {
+    type: String,
+    required: 'Please enter a last name.'
+  },
+  type: {
+    type: String,
+    required: 'Please enter a type.'
+  },
+  onLeave: {
+    type: Boolean,
+    required: 'Please say whether or not this person is on leave.'
+  },
+  specialties: {
+    type: String,
+    required: 'Please enter this persons specialties.'
+  },
+  biography: {
+    type: String,
+    required: 'Please enter a biography.'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Users', UserSchema);
